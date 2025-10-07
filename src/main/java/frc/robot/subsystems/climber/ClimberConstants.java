0594cc328ddc07e8852983cca5e5d49b0b12cd91
DarkEden-Coding <darkedenc9@gmail.com>
2025-10-07 17:32:04 -0400

    Complete climber contants

--- src/main/java/frc/robot/subsystems/climber/ClimberConstants.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberConstants.java
@@ -1,3 +1,16 @@
 package frc.robot.subsystems.climber;
 
-public class ClimberConstants {}
+public abstract class ClimberConstants {
+    // Climber motor CAN ID
+    // Pid constants
+    // setpoints
+    public static final int CLIMBER_MOTOR_CAN_ID = 10;
+
+    public static final double CLIMBER_PID_P = 0.1;
+    public static final double CLIMBER_PID_I = 0.0;
+    public static final double CLIMBER_PID_D = 0.0;
+
+    public static final double CLIMBER_SETPOINT_LOW = 0.0;
+    public static final double CLIMBER_SETPOINT_CLIMB = 100.0;
+    public static final double CLIMBER_SETPOINT_STOW = 50.0;
+}
